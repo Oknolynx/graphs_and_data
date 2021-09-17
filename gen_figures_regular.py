02ee119952370accdf7b35c173a6a320209629b0
Max Ihlenfeldt <max@ihlenfeldt.net>
2021-09-17 14:33:32 +0200

    nicer looking legends, change font to Charter

--- gen_figures_regular.py
+++ gen_figures_regular.py
@@ -3,7 +3,6 @@
 import json
 import matplotlib.pyplot as plt
 import numpy as np
-import os.path
 import re
 from os import listdir
 from os.path import basename, isdir, join
@@ -11,6 +10,16 @@ from os.path import basename, isdir, join
 
 JSON_FOLDER = 'json'
 FIGURES_FOLDER = 'figures'
+FIGURE_SIZE = (6.4, 3)
+FONT_OPTIONS = {
+    'family': 'serif',
+    'serif': 'Charter',
+    'style': 'normal',
+    'variant': 'normal',
+    'weight': 'normal',
+    'stretch': 'normal',
+    'size': 10
+}
 REGEX_GROUPS = ['mode', 'blocksize', 'driver', 'disk', 'suffix', 'number']
 
 
@@ -57,8 +66,8 @@ def filename_from_group_vals(mode, blocksize, driver, disk, suffix, number=''):
     '''
     reconstructs a filename from its regex group values
     '''
-    filename = f"{mode}_{blocksize}_{driver}_{disk}{suffix}"
-    if number != '':
+    filename = f'{mode}_{blocksize}_{driver}_{disk}{suffix}'
+    if (number != '') and (number is not None):
         filename += '_' + number
     return filename + '.json'
 
@@ -75,6 +84,8 @@ def nice_disk_name(disk):
         return 'SSD'
     elif disk == 'hdd':
         return 'HDD'
+    elif disk == 'vmssd':
+        return 'SSD in VM'
 
 
 def nice_driver_name(driver):
@@ -82,9 +93,9 @@ def nice_driver_name(driver):
         return 'BitLocker'
     elif driver == 'veracrypt':
         return 'VeraCrypt'
-    elif driver == 'luks2flt-optimizedv2':
+    elif (driver == 'luks2flt-optimizedv2') or (driver == 'luks2flt-optimized'):
         return 'luks2flt (optimized)'
-    elif driver == 'luks2flt-beforemoreopts':
+    elif (driver == 'luks2flt') or (driver == 'luks2flt-beforemoreopts'):
         return 'luks2flt'
     elif driver == 'nullcrypto-disabled':
         return 'Regular'
@@ -92,35 +103,36 @@ def nice_driver_name(driver):
         return 'Nullcrypto'
 
 
-def generate_bandwidth_graphs(bandwidth_data, driver_names, block_sizes, mode, disk, ax):
+def generate_throughput_graphs(throughput_data, driver_names, block_sizes, mode, disk, ax):
     '''
     parameters:
-        - bandwidth_data: list that contains the bandwidths of the different drivers
+        - throughput_data: list that contains the throughputs of the different drivers
         (these are lists themselves)
         - driver_names: list of the driver names in the same order as the data in
-        `bandwidth_data`
-        - block_sizes: list of the block sizes that the bandwidths correspond to
-        in the same order as the data in `bandwidth_data`
+        `throughput_data`
+        - block_sizes: list of the block sizes that the throughputs correspond to
+        in the same order as the data in `throughput_data`
         - disk: name of the disk (e.g. 'hdd' or 'ssd')
         - mode: mode of the data (e.g. 'seqread' or 'randread')
         - ax: matplotlib axis used for plotting
     '''
     strip_k_suffix = lambda bs: bs[:-1]
     block_sizes = list(map(strip_k_suffix, block_sizes))
-    for driver, data in zip(driver_names, bandwidth_data):
+    for driver, data in zip(driver_names, throughput_data):
         ax.plot(block_sizes, data, label=nice_driver_name(driver))
     if mode == 'seqread':
-        ax.legend(loc='lower left')
+        columns = 3 if len(driver_names) == 3 else 2
+        ax.legend(loc='lower left', frameon=False, ncol=columns)
     elif mode == 'randread':
-        ax.legend(loc='upper left')
+        ax.legend(loc='upper left', frameon=False)
     ax.set_title(nice_mode_name(mode) + f' ({nice_disk_name(disk)})')
     ax.set_xlabel('Block size [KiB]')
     ax.set_ylabel('Throughput [MiB/s]')
 
 
-def get_bandwidth_data(json_path):
+def get_throughput_data(json_path):
     '''
-    reads the bandwidth from a given json path
+    reads the throughput from a given json path
     parameters:
         - json_path: path to the JSON file to read the data from
     '''
@@ -144,15 +156,15 @@ def sort_blocksizes(blocksizes):
     return list(map(map_func, blocksizes))
 
 
-def average_bandwidth(directory, mode, bs, driver, disk, suffix, numbers):
+def average_throughput(directory, mode, bs, driver, disk, suffix, numbers):
     '''
-    gets the average bandwidth from json files with all the same regex group values except for different numbers
+    gets the average throughput from json files with all the same regex group values except for different numbers
     '''
     bw_sum = 0
     for number in numbers:
         json_file = filename_from_group_vals(mode, bs, driver, disk, suffix, number)
-        json_path = os.path.join(directory, json_file)
-        bw_sum += get_bandwidth_data(json_path)
+        json_path = join(directory, json_file)
+        bw_sum += get_throughput_data(json_path)
     return bw_sum / len(numbers)
 
 
@@ -176,7 +188,7 @@ def handle_directory(directory):
         for mode in modes:
             final_data = {disk: np.empty((len(drivers), len(blocksizes))) for disk in disks}
             figure_path = join(FIGURES_FOLDER, f'{basename(directory)}_{mode}{suffix}.pdf')
-            fig = plt.figure(figsize=(10, 8))
+            fig = plt.figure(figsize=(FIGURE_SIZE[0], FIGURE_SIZE[1]*len(disks)))
             axes = fig.subplots(len(disks), 1)
             print(f'generating figure \'{figure_path}\'')
 
@@ -184,19 +196,21 @@ def handle_directory(directory):
                 for ii, driver in enumerate(drivers):
                     data = np.empty(len(blocksizes))
                     for iii, bs in enumerate(blocksizes):
-                        data[iii] = average_bandwidth(directory, mode, bs, driver, disk, suffix, numbers)
+                        data[iii] = average_throughput(directory, mode, bs, driver, disk, suffix, numbers)
                         
                     final_data[disk][ii] = data / 1024 / 1024
 
-                axis_index = i
-                generate_bandwidth_graphs(final_data[disk], drivers, blocksizes, mode, disk, axes[axis_index])
-                # plt.setp(axes[axis_index].get_xticklabels(), ha="right", rotation=45)
-                axes[axis_index].set_ylim(bottom=0)
+                ax = axes[i] if len(disks) > 1 else axes
+                generate_throughput_graphs(final_data[disk], drivers, blocksizes, mode, disk, ax)
+                # plt.setp(ax.get_xticklabels(), ha="right", rotation=45)
+                ax.set_ylim(bottom=0)
 
             fig.tight_layout()
             fig.savefig(figure_path, bbox_inches='tight')
 
+
 if __name__ == '__main__':
+    plt.rc('font', **FONT_OPTIONS)
     for maybe_dir in listdir(JSON_FOLDER):
         full_path = join(JSON_FOLDER, maybe_dir)
         if not isdir(full_path):
