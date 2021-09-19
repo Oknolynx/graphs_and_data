8b205db94f05fcc1ba869a0f1534c3634d666d08
Max Ihlenfeldt <max@ihlenfeldt.net>
2021-09-19 21:49:06 +0200

    update .gitignore, add rename script

---
+++ figures/rename.bat
@@ -0,0 +1,22 @@
+rename "VM_seqread.pdf" "performance.vmexperiments.seq.pdf"
+rename "VM_randread.pdf" "performance.vmexperiments.rand.pdf"
+
+rename "nullcrypto_seqread_numjobs8.pdf" "performance.hwexperiments.nullcipherseq.pdf"
+rename "nullcrypto_randread_numjobs8.pdf" "performance.hwexperiments.nullcipherrand.pdf"
+
+rename "before more aggressive opts iodepth 1+numjobs 1_seqread.pdf" "performance.hwexperiments.beforeoptseq.pdf"
+rename "before more aggressive opts iodepth 1+numjobs 1_randread.pdf" "performance.hwexperiments.beforeoptrand.pdf"
+
+rename "optimizedv2 all iodepth and numjobs combos_seqread_iodepth1_numjobs1.pdf" "performance.hwexperiments.optseq.pdf"
+rename "optimizedv2 all iodepth and numjobs combos_randread_iodepth1_numjobs1.pdf" "performance.hwexperiments.optrand.pdf"
+rename "optimizedv2 all iodepth and numjobs combos_seqread_iodepth16numjobs1.pdf" "performance.hwexperiments.optseqqueue.pdf"
+rename "optimizedv2 all iodepth and numjobs combos_randread_iodepth16numjobs1.pdf" "performance.hwexperiments.optrandqueue.pdf"
+rename "optimizedv2 all iodepth and numjobs combos_seqread_iodepth1numjobs16.pdf" "performance.hwexperiments.optseqthreads.pdf"
+rename "optimizedv2 all iodepth and numjobs combos_randread_iodepth1numjobs16.pdf" "performance.hwexperiments.optrandthreads.pdf"
+rename "optimizedv2 all iodepth and numjobs combos_seqread_iodepth16_numjobs16.pdf" "performance.hwexperiments.optseqthreadsqueue.pdf"
+rename "optimizedv2 all iodepth and numjobs combos_randread_iodepth16_numjobs16.pdf" "performance.hwexperiments.optrandthreadsqueue.pdf"
+
+rename "json_throughput_log_seqread_256k_logmsec16iodepth1.pdf" "performance.hwexperiments.seqovertime.pdf"
+rename "json_throughput_log_randread_256k_logmsec16iodepth1.pdf" "performance.hwexperiments.randovertime.pdf"
+rename "json_throughput_log_seqread_256k_logmsec16iodepth1_boxplot.pdf" "performance.hwexperiments.seqovertimebox.pdf"
+rename "json_throughput_log_randread_256k_logmsec16iodepth1_boxplot.pdf" "performance.hwexperiments.randovertimebox.pdf"
